5ad9972aa4a07f4dc91a58c63ed288b36f9a6c08
SimplStudios <simplstudios@protonmail.com>
2026-03-23 18:10:24 -0500

    Delete src directory

--- src/lib/formatDuration.ts
+++
@@ -1,8 +0,0 @@
-export function formatDuration(minutes: number): string {
-  const hours = Math.floor(minutes / 60);
-  const mins = minutes % 60;
-  if (hours > 0) {
-    return `${hours}h ${mins}m`;
-  }
-  return `${mins}m`;
-}
